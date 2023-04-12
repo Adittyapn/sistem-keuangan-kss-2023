216afd8bf622d8ca4c565fef6c9c239d4ca01173
Adittyapn <adityapn2731@gmail.com>
2023-04-12 21:53:07 +0700

    add keuangan-pnrm

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 /*? no js js needed from me */
+function restrictAlpha(e) {
+    var x = e.which || e.keycode;
+    if ((x >= 48 && x <= 57))
+        return true;
+    else
+        return false;
+}
