77c291e4c454d9a1873f8d0484c1eaed9dc8e384
Doodle6660 <34979469+Doodle6660@users.noreply.github.com>
2023-04-17 16:59:17 +0700

    modify stucture (adding new table)

--- keuangan.sql
+++ keuangan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 14 Apr 2023 pada 22.36
--- Versi server: 8.0.30
--- Versi PHP: 8.1.10
+-- Generation Time: Apr 17, 2023 at 09:58 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `login`
+-- Table structure for table `login`
 --
 
 CREATE TABLE `login` (
@@ -35,22 +35,54 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `login`
+-- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`nik`, `username`, `nama_pengguna`, `pass`) VALUES
 ('3411201102', '123', 'paw', '123'),
 ('3411201105', '123', '123', '132');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penerimaan_kas`
+--
+
+CREATE TABLE `penerimaan_kas` (
+  `id` int NOT NULL,
+  `payment` varchar(255) NOT NULL,
+  `no_invoice` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `caraBayar` varchar(255) NOT NULL,
+  `kodeBank` varchar(255) NOT NULL,
+  `noRekening` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `login`
+-- Indexes for table `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`nik`);
+
+--
+-- Indexes for table `penerimaan_kas`
+--
+ALTER TABLE `penerimaan_kas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `penerimaan_kas`
+--
+ALTER TABLE `penerimaan_kas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
